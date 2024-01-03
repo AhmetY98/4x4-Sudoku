92caa085815797305bbd4102b5b5387ce4dfc592
AhmetY98 <73417190+AhmetY98@users.noreply.github.com>
2024-01-02 16:34:34 -0800

    Update 4x4TicTacToe.py

--- 4x4TicTacToe.py
+++ 4x4TicTacToe.py
@@ -115,49 +115,100 @@ def winningCases(board):
     input1 = "X"
     input2 = "O"
 
-    #Using a nested loop for checking winning conditions        
     # Check rows and columns
-    for i in range(4):
-        if all(board[i * 4 + j] == input1 for j in range(4)) or all(board[i + j * 4] == input1 for j in range(4)):
-            return True
-        elif all(board[i * 4 + j] == input2 for j in range(4)) or all(board[i + j * 4] == input2 for j in range(4)):
-            return True
-
-    # Check diagonals
-    if all(board[i * 5] == input1 for i in range(4)) or all(board[i * 5] == input2 for i in range(4)):
-        return True
-    elif all(board[i * 3 + 3] == input1 for i in range(4)) or all(board[i * 3 + 3] == input2 for i in range(4)):
-        return True
-
-    return False
-
-#Inserting every winning condition one by one
-##    if (
-##            board[0] == input1 and board[1] == input1 and board[2] == input1 and board[4] == input1 or board[4] == input1 and board[5] == input1 and board[6] == input1 and board[7] == input1 or
-##            board[8] == input1 and board[9] == input1 and board[10] == input1 and board[11] == input1 or board[12] == input1 and board[13] == input1 and board[14] == input1 and board[15] == input1 or
-##            board[0] == input1 and board[4] == input1 and board[8] == input1 and board[12] == input1 or board[1] == input1 and board[5] == input1 and board[9] == input1 and board[13] == input1 or
-##            board[2] == input1 and board[6] == input1 and board[10] == input1 and board[14] == input1 or board[3] == input1 and board[7] == input1 and board[11] == input1 and board[15] == input1 or
-##            board[0] == input1 and board[5] == input1 and board[10] == input1 and board[15] == input1 or board[3] == input1 and board[6] == input1 and board[9] == input1 and board[12] == input1 or
-##            board[1] == input1 and board[6] == input1 and board[11] == input1 or board[2] == input1 and board[5] == input1 and board[8] == input1 or board[4] == input1 and board[9] == input1 and board[14] == input1 or
-##            board[7] == input1 and board[10] == input1 and board[13] == input1):
+##    for i in range(4):
+##        if all(board[i * 4 + j] == input1 for j in range(4)) or all(board[i + j * 4] == input1 for j in range(4)):
+##            return True
+##        elif all(board[i * 4 + j] == input2 for j in range(4)) or all(board[i + j * 4] == input2 for j in range(4)):
+##            return True
 ##
+##    # Check diagonals
+##    if all(board[i * 5] == input1 for i in range(4)) or all(board[i * 5] == input2 for i in range(4)):
 ##        return True
-##    elif (board[0] == input2 and board[1] == input2 and board[2] == input2 and board[4] == input2 or board[4] == input2 and board[5] == input2 and board[6] == input2 and board[7] == input2 or
-##          board[8] == input2 and board[9] == input2 and board[10] == input2 and board[11] == input2 or board[12] == input2 and board[13] == input2 and board[14] == input2 and board[15] == input2 or
-##          board[0] == input2 and board[4] == input2 and board[8] == input2 and board[12] == input2 or board[1] == input2 and board[5] == input2 and board[9] == input2 and board[13] == input2 or
-##          board[2] == input2 and board[6] == input2 and board[10] == input2 and board[14] == input2 or board[3] == input2 and board[7] == input2 and board[11] == input2 and board[15] == input2 or
-##          board[0] == input2 and board[5] == input2 and board[10] == input2 and board[15] == input2 or board[3] == input2 and board[6] == input2 and board[9] == input2 and board[12] == input2 or
-##          board[1] == input2 and board[6] == input2 and board[11] == input2 or board[2] == input2 and board[5] == input2 and board[8] == input2 or board[4] == input2 and board[9] == input2 and board[14] == input2 or
-##          board[7] == input2 and board[10] == input2 and board[13] == input2):
+##    elif all(board[i * 3 + 3] == input1 for i in range(4)) or all(board[i * 3 + 3] == input2 for i in range(4)):
 ##        return True
-##    else:
-##        return False
+##
+##    return False
+
+
+    if (
+            board[0] == input1 and board[1] == input1 and board[2] == input1 and board[4] == input1 or board[4] == input1 and board[5] == input1 and board[6] == input1 and board[7] == input1 or
+            board[8] == input1 and board[9] == input1 and board[10] == input1 and board[11] == input1 or board[12] == input1 and board[13] == input1 and board[14] == input1 and board[15] == input1 or
+            board[0] == input1 and board[4] == input1 and board[8] == input1 and board[12] == input1 or board[1] == input1 and board[5] == input1 and board[9] == input1 and board[13] == input1 or
+            board[2] == input1 and board[6] == input1 and board[10] == input1 and board[14] == input1 or board[3] == input1 and board[7] == input1 and board[11] == input1 and board[15] == input1 or
+            board[0] == input1 and board[5] == input1 and board[10] == input1 and board[15] == input1 or board[3] == input1 and board[6] == input1 and board[9] == input1 and board[12] == input1 or
+            board[1] == input1 and board[6] == input1 and board[11] == input1 or board[2] == input1 and board[5] == input1 and board[8] == input1 or board[4] == input1 and board[9] == input1 and board[14] == input1 or
+            board[7] == input1 and board[10] == input1 and board[13] == input1):
+
+        return True
+    elif (board[0] == input2 and board[1] == input2 and board[2] == input2 and board[4] == input2 or board[4] == input2 and board[5] == input2 and board[6] == input2 and board[7] == input2 or
+          board[8] == input2 and board[9] == input2 and board[10] == input2 and board[11] == input2 or board[12] == input2 and board[13] == input2 and board[14] == input2 and board[15] == input2 or
+          board[0] == input2 and board[4] == input2 and board[8] == input2 and board[12] == input2 or board[1] == input2 and board[5] == input2 and board[9] == input2 and board[13] == input2 or
+          board[2] == input2 and board[6] == input2 and board[10] == input2 and board[14] == input2 or board[3] == input2 and board[7] == input2 and board[11] == input2 and board[15] == input2 or
+          board[0] == input2 and board[5] == input2 and board[10] == input2 and board[15] == input2 or board[3] == input2 and board[6] == input2 and board[9] == input2 and board[12] == input2 or
+          board[1] == input2 and board[6] == input2 and board[11] == input2 or board[2] == input2 and board[5] == input2 and board[8] == input2 or board[4] == input2 and board[9] == input2 and board[14] == input2 or
+          board[7] == input2 and board[10] == input2 and board[13] == input2):
+        return True
+    else:
+        return False
 
 #state a number for turns
 starter = determineFirstMove()
 checker=starter
 
+class ComputerPlayType:
+    def __init__(self,symbol,difficulty):
+        self.symbol=symbol # Either X or O
+        self.difficulty=difficulty
 
+    def make_move(self,board):
+        _,move=self.minimax(board,self.symbol,depth_level=self.difficulty) #assigining the move to the symbol
+        return move
+    def minimax(self,board, gamer, depth_level):
+        #checking available moves by looking at the values if they are number or not
+        available_moves=[i for i, value in enumerate(board) if value.isdigit()]
+
+        if(winningCases(board)):
+            if gamer == self.symbol:
+                return -1,None #let the player win
+            else:
+                return 1,None #let the computer win
+        elif tieGame(board):
+            return 0,None #The game is a tie
+
+
+        #To check which depth we are at we need a depth limit
+        if depth_level==0:
+            return 0,None #when the depth level reaches 0 we will send neutral score
+
+        #To maximize the computer score
+        if gamer==self.symbol:
+            best_score=float('-inf')
+            best_move=None
+            for move in available_moves:
+                new_board=board.copy()
+                new_board[move]=player
+                score , _ =self.minimax(new_board,"X" if gamer=="O" else "O",depth_level-1)
+                if score>best_score:
+                    best_score=score
+                    best_move=move
+            return best_score,best_move
+            
+        #To minimize the player score
+        else:
+            best_score=float('inf')
+            best_move=None
+            for move in available_moves:
+                new_board=board.copy()
+                new_board[move]=gamer
+                score,_=self.minimax(new_board,"X" if gamer=="O" else "O",depth_level-1)
+                if score< best_score:
+                    best_score=score
+                    best_move=move
+            return best_score,best_move
+            
+
+           
 
 def main():
     global starter
@@ -165,6 +216,14 @@ def main():
     printTheBoard(board2)
     conditionWinning = winningCases(board2)
     noWinner=True
+
+    #ask for difficutly
+    difficulty=input("Choose the difficulty level by entering easy, medium or hard (will be medium if nothing is entered): ")
+    difficulty_level={"easy":1,"medium":3,"hard":5}
+    depth_level=difficulty_level.get(difficulty.lower(),3)
+
+    computer_player=ComputerPlayType("O" if checker==1 else "X",depth_level)
+    
     
     if len(boardX) != 0 and noWinner == True:
         while noWinner == True:
